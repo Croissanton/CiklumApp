a464efd15d1d93c69d28fe0c9e1ab2e38b42bec6
p <pabloruar03@uma.es>
2023-12-11 23:10:44 +0100

    sesion 👌👍

--- CiklumApp/Sesion.Designer.cs
+++ CiklumApp/Sesion.Designer.cs
@@ -57,11 +57,10 @@ namespace CiklumApp
             this.Nombre,
             this.Repeticiones,
             this.Series});
-            this.dgvSesion.Location = new System.Drawing.Point(489, 265);
-            this.dgvSesion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvSesion.Location = new System.Drawing.Point(367, 215);
             this.dgvSesion.Name = "dgvSesion";
             this.dgvSesion.RowHeadersWidth = 51;
-            this.dgvSesion.Size = new System.Drawing.Size(768, 370);
+            this.dgvSesion.Size = new System.Drawing.Size(576, 301);
             this.dgvSesion.TabIndex = 19;
             this.dgvSesion.DoubleClick += new System.EventHandler(this.dgvSesion_DoubleClick);
             // 
@@ -86,9 +85,10 @@ namespace CiklumApp
             // bSesiones
             // 
             this.bSesiones.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.bSesiones.Location = new System.Drawing.Point(112, 571);
+            this.bSesiones.Location = new System.Drawing.Point(84, 464);
+            this.bSesiones.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bSesiones.Name = "bSesiones";
-            this.bSesiones.Size = new System.Drawing.Size(208, 53);
+            this.bSesiones.Size = new System.Drawing.Size(156, 43);
             this.bSesiones.TabIndex = 18;
             this.bSesiones.Text = "SESIONES";
             this.bSesiones.UseVisualStyleBackColor = true;
@@ -96,9 +96,10 @@ namespace CiklumApp
             // bClientes
             // 
             this.bClientes.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.bClientes.Location = new System.Drawing.Point(112, 455);
+            this.bClientes.Location = new System.Drawing.Point(84, 370);
+            this.bClientes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bClientes.Name = "bClientes";
-            this.bClientes.Size = new System.Drawing.Size(208, 53);
+            this.bClientes.Size = new System.Drawing.Size(156, 43);
             this.bClientes.TabIndex = 17;
             this.bClientes.Text = "CLIENTES";
             this.bClientes.UseVisualStyleBackColor = true;
@@ -107,9 +108,10 @@ namespace CiklumApp
             // bEjercicios
             // 
             this.bEjercicios.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.bEjercicios.Location = new System.Drawing.Point(112, 335);
+            this.bEjercicios.Location = new System.Drawing.Point(84, 272);
+            this.bEjercicios.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bEjercicios.Name = "bEjercicios";
-            this.bEjercicios.Size = new System.Drawing.Size(208, 53);
+            this.bEjercicios.Size = new System.Drawing.Size(156, 43);
             this.bEjercicios.TabIndex = 16;
             this.bEjercicios.Text = "EJERCICIOS";
             this.bEjercicios.UseVisualStyleBackColor = true;
@@ -118,9 +120,10 @@ namespace CiklumApp
             // bHome
             // 
             this.bHome.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.bHome.Location = new System.Drawing.Point(112, 229);
+            this.bHome.Location = new System.Drawing.Point(84, 186);
+            this.bHome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bHome.Name = "bHome";
-            this.bHome.Size = new System.Drawing.Size(208, 53);
+            this.bHome.Size = new System.Drawing.Size(156, 43);
             this.bHome.TabIndex = 15;
             this.bHome.Text = "HOME";
             this.bHome.UseVisualStyleBackColor = true;
@@ -131,20 +134,19 @@ namespace CiklumApp
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.25F);
-            this.label1.Location = new System.Drawing.Point(617, 49);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(463, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(289, 78);
+            this.label1.Size = new System.Drawing.Size(231, 63);
             this.label1.TabIndex = 22;
             this.label1.Text = "SESION";
             // 
             // bBorrar
             // 
             this.bBorrar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.bBorrar.Location = new System.Drawing.Point(520, 544);
+            this.bBorrar.Location = new System.Drawing.Point(519, 544);
             this.bBorrar.Margin = new System.Windows.Forms.Padding(2);
             this.bBorrar.Name = "bBorrar";
-            this.bBorrar.Size = new System.Drawing.Size(175, 53);
+            this.bBorrar.Size = new System.Drawing.Size(131, 43);
             this.bBorrar.TabIndex = 28;
             this.bBorrar.Text = "Borrar";
             this.bBorrar.UseVisualStyleBackColor = true;
@@ -156,7 +158,7 @@ namespace CiklumApp
             this.bAñadir.Location = new System.Drawing.Point(367, 544);
             this.bAñadir.Margin = new System.Windows.Forms.Padding(2);
             this.bAñadir.Name = "bAñadir";
-            this.bAñadir.Size = new System.Drawing.Size(165, 54);
+            this.bAñadir.Size = new System.Drawing.Size(124, 44);
             this.bAñadir.TabIndex = 27;
             this.bAñadir.Text = "Añadir";
             this.bAñadir.UseVisualStyleBackColor = true;
@@ -170,11 +172,10 @@ namespace CiklumApp
             this.dgvSesiones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ID,
             this.NombreSesion});
-            this.dgvSesiones.Location = new System.Drawing.Point(1313, 270);
-            this.dgvSesiones.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvSesiones.Location = new System.Drawing.Point(985, 219);
             this.dgvSesiones.Name = "dgvSesiones";
             this.dgvSesiones.RowHeadersWidth = 51;
-            this.dgvSesiones.Size = new System.Drawing.Size(259, 364);
+            this.dgvSesiones.Size = new System.Drawing.Size(194, 296);
             this.dgvSesiones.TabIndex = 29;
             this.dgvSesiones.Click += new System.EventHandler(this.dgvSesiones_Click);
             // 
@@ -184,21 +185,20 @@ namespace CiklumApp
             this.ID.MinimumWidth = 6;
             this.ID.Name = "ID";
             this.ID.Visible = false;
-            this.ID.Width = 125;
             // 
             // NombreSesion
             // 
             this.NombreSesion.HeaderText = "Sesion";
             this.NombreSesion.MinimumWidth = 6;
             this.NombreSesion.Name = "NombreSesion";
-            this.NombreSesion.Width = 125;
             // 
             // borrarSesion
             // 
-            this.borrarSesion.Location = new System.Drawing.Point(1372, 741);
-            this.borrarSesion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.borrarSesion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.borrarSesion.Location = new System.Drawing.Point(1029, 602);
+            this.borrarSesion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.borrarSesion.Name = "borrarSesion";
-            this.borrarSesion.Size = new System.Drawing.Size(165, 53);
+            this.borrarSesion.Size = new System.Drawing.Size(124, 43);
             this.borrarSesion.TabIndex = 31;
             this.borrarSesion.Text = "Borrar";
             this.borrarSesion.UseVisualStyleBackColor = true;
@@ -206,10 +206,11 @@ namespace CiklumApp
             // 
             // añadirSesion
             // 
-            this.añadirSesion.Location = new System.Drawing.Point(1372, 669);
-            this.añadirSesion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.añadirSesion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.añadirSesion.Location = new System.Drawing.Point(1029, 544);
+            this.añadirSesion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.añadirSesion.Name = "añadirSesion";
-            this.añadirSesion.Size = new System.Drawing.Size(165, 54);
+            this.añadirSesion.Size = new System.Drawing.Size(124, 44);
             this.añadirSesion.TabIndex = 30;
             this.añadirSesion.Text = "Añadir";
             this.añadirSesion.UseVisualStyleBackColor = true;
@@ -217,9 +218,9 @@ namespace CiklumApp
             // 
             // Sesion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1588, 842);
+            this.ClientSize = new System.Drawing.Size(1191, 684);
             this.Controls.Add(this.borrarSesion);
             this.Controls.Add(this.añadirSesion);
             this.Controls.Add(this.dgvSesiones);
@@ -231,7 +232,6 @@ namespace CiklumApp
             this.Controls.Add(this.bClientes);
             this.Controls.Add(this.bEjercicios);
             this.Controls.Add(this.bHome);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Sesion";
             this.Text = "z";
             this.Load += new System.EventHandler(this.Sesion_Load_1);
