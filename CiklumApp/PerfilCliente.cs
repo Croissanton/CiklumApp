592ae0aa06123e42bef8ce6f087c7275ee703b23
unknown <crisruizmartin1@gmail.com>
2023-12-18 20:35:17 +0100

    perfil cliente pone entrenadores e

--- CiklumApp/PerfilCliente.cs
+++ CiklumApp/PerfilCliente.cs
@@ -17,8 +17,6 @@ namespace CiklumApp
         {
             InitializeComponent();
             initMenu();
-
-            setData();
         }
 
         private void initMenu()
@@ -37,22 +35,6 @@ namespace CiklumApp
             }
 
             cbMenu.SelectedIndex = 1;
-        }
-
-
-
-        private void setData()
-        {
-            var consulta = new Consulta();
-            var datosCliente = consulta.Select($"SELECT * FROM CLIENTE WHERE ID_USUARIO = {Login.user.ID()}")[0];
-            tbNombre.Text = (string) datosCliente[1];
-            tbApellidos.Text = (string) datosCliente[2];
-            lNombreCliente.Text = tbNombre.Text + " " + tbApellidos.Text;
-            tbAltura.Text = Math.Round((Convert.ToDouble(datosCliente[6])), 2).ToString();
-            tbPeso.Text = Math.Round((Convert.ToDouble(datosCliente[5])), 2).ToString();
-
-            var IMC = Math.Round(Convert.ToDouble(tbPeso.Text) / (Convert.ToDouble(tbAltura.Text) / 100 * Convert.ToDouble(tbAltura.Text) / 100), 2);
-            tbIMC.Text = IMC.ToString();
         }
 
 
@@ -100,6 +82,30 @@ namespace CiklumApp
             this.Close();
             PerfilCliente perfilCliente = new PerfilCliente();
             perfilCliente.Show();
+        }
+
+        private void PerfilCliente_Load(object sender, EventArgs e)
+        {
+            var consulta = new Consulta();
+            var datosCliente = consulta.Select($"SELECT * FROM CLIENTE WHERE ID_USUARIO = {Login.user.ID()}")[0];
+            tbNombre.Text = (string)datosCliente[1];
+            tbApellidos.Text = (string)datosCliente[2];
+            lNombreCliente.Text = tbNombre.Text + " " + tbApellidos.Text;
+            tbAltura.Text = Math.Round((Convert.ToDouble(datosCliente[6])), 2).ToString();
+            tbPeso.Text = Math.Round((Convert.ToDouble(datosCliente[5])), 2).ToString();
+
+            var IMC = Math.Round(Convert.ToDouble(tbPeso.Text) / (Convert.ToDouble(tbAltura.Text) / 100 * Convert.ToDouble(tbAltura.Text) / 100), 2);
+            tbIMC.Text = IMC.ToString();
+            var list = consulta.Select("SELECT DISTINCT E.NOMBRE, E.APELLIDOS, E.ESPECIALIDAD FROM ENTRENADOR E " +
+            "JOIN CLIENTE_ENTRENADOR C_E ON E.ID_USUARIO = C_E.ID_ENTRENADOR " +
+            "WHERE C_E.ID_CLIENTE = " + Login.user.ID() + ";");
+            //Create an array for storing numbers
+            Console.WriteLine(list.Count);
+            foreach (var item in list)
+            {
+                this.dgvEntrenadores.Rows.Add((string)(item[0]), (string)item[1], (string)item[2]);
+            }
+            dgvEntrenadores.ClearSelection();
         }
     }
 }
