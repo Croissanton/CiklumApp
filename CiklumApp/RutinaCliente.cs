724e6f603a7c3992241f6c7e8ada617ad553ff4b
unknown <crisruizmartin1@gmail.com>
2023-12-18 22:00:06 +0100

    PerfilEntrenador con cosillas23

--- CiklumApp/RutinaCliente.cs
+++ CiklumApp/RutinaCliente.cs
@@ -287,7 +287,6 @@ namespace CiklumApp
 
         private void bFeedbackLunes_Click(object sender, EventArgs e)
         {
-            Fe
         }
 
         private void bFeedbackMartes_Click(object sender, EventArgs e)
